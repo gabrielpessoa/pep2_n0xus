32e01b856a0b0c6432dd0b17e05adfa0fd3ce295
arthurviniciuse <arthur.alucard41@gmail.com>
2019-04-21 22:10:13 -0300

    #23 correção da pasta de arquivos do projeto, ok

--- Studio/config/app.php
+++ Studio/config/app.php
@@ -2,9 +2,6 @@
 
 return [
 
-Way\Generators\GeneratorsServiceProvider::class,
-Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
