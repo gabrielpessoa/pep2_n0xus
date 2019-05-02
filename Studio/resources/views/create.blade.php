9315e9567ef47fbb9b5d7eb280663fbf7798d5e3
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-05-01 21:24:53 -0300

    grupo muscular

---
+++ Studio/resources/views/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Adicionar novo grupo </h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('gmuscular.index') }}"> Voltar</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('gmuscular.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nome:</strong>
+                <input type="text" name="nome" class="form-control" placeholder="Tríceps">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Exercicío:</strong>
+                <textarea class="form-control" style="height:150px" name="exercicio" placeholder="Legging"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Adicionar</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ Studio/resources/views/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Visualizar grupo</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('gmuscular.index') }}"> Voltar</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nome:</strong>
+                {{ $gmuscular->nome }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Exercício:</strong>
+                {{ $gmuscular->exercicio }}
+            </div>
+        </div>
+    </div>
+@endsection
