176dadbd7d4083d06ff0c60a8b159079934ab665
GuilhermeEvaristo <guilherme.evaristo.paz@gmail.com>
2019-05-06 17:30:54 -0300

    Realocando projeto para a pasta correta

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        factory('App\User', 50)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'Admin@Admin.com',
+            'tipo'=> 'gerente',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => 1
+        ]);
+    }
+    
+}
