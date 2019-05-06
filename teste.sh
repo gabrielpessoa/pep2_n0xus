e60578e222673d322d7f76dd4de91fa68acfee9a
GuilhermeEvaristo <guilherme.evaristo.paz@gmail.com>
2019-05-06 17:52:14 -0300

    Revert "Caso alguém commite com o nome de outra pessoa"
    This reverts commit 92a4c04eacda8abe326ccae5755671d9d7725ea0.

--- teste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git filter-branch -f --commit-filter 'if [ "$GIT_AUTHOR_NAME" = "joselimabar" ];
-then export GIT_AUTHOR_NAME="GuilhermeEvaristo"; export GIT_AUTHOR_EMAIL=guilherme.evaristo.paz@gmail.com;
-fi; git commit-tree "$@"';
-
-git filter-branch -f --commit-filter 'if [ "$GIT_COMMITTER_NAME" = "joselimabar" ];
-then export GIT_COMMITTER_NAME="GuilhermeEvaristo"; export GIT_COMMITTER_EMAIL=guilherme.evaristo.paz@gmail.com;
-fi; git commit-tree "$@"';
