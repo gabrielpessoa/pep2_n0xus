19f5f93e6f1ccae0343e465e8b4a2e78406554fc
arthurviniciuse <arthur.alucard41@gmail.com>
2019-05-08 12:09:47 -0300

    #31 Controller de turma

---
+++ app/Http/Controllers/TurmaController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use App\Turma;
+use Illuminate\Http\Request;
+use App\Http\Requests\TurmaRequest;
+class TurmaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('turma.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('turma.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TurmaRequest $request)
+    {
+        $insert = new Turma;
+        $insert->nome = $request->nome;
+        $insert->horario= $request->horario;
+        $insert->user_id = Auth::id();
+        $insert->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
