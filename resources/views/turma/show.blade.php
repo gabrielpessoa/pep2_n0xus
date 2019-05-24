b5a361033813cdee4544554586b3b0d0ad99688c
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-05-24 14:35:58 -0300

    #31 modificando o show da turma

--- resources/views/turma/show.blade.php
+++ resources/views/turma/show.blade.php
@@ -1,12 +1,12 @@
 @extends('adminlte::page')
 @section('content')
-    <div class="row">
+    <div class="row"> 
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Visualizar grupo</h2>
+                <h2> Visualizar turma</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('gmuscular.index') }}"> Voltar</a>
+                <a class="btn btn-primary" href="{{ route('turma.index') }}"> Voltar</a>
             </div>
         </div>
     </div>
@@ -15,13 +15,13 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Nome:</strong>
-                {{ $gmusculares->nome }}
+                {{ $turmas->nome }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Exercício:</strong>
-                {{ $gmusculares->exercicio }}
+                <strong>Horário:</strong>
+                {{ $turmas->horario }}
             </div>
         </div>
     </div>
