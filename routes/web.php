21e009f3c9c731c41854a2d8eb6536c9b13212bc
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-05-29 17:44:35 -0300

    #44 criando rota para equipamentos

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route  resource('/equipamento', 'EquipamentoController');
 
 //Rota para adicionar grupo muscular
 Route::resource('/gmuscular','GmuscularController');
