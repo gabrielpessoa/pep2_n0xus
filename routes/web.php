df1274872bb94cf663104e74a37339ce62a82302
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-05-29 18:20:30 -0300

    #44 corrigindo rota de equipamento

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route  resource('/equipamento', 'EquipamentoController');
+Route::resource('/equipamento', 'EquipamentoController');
 
 //Rota para adicionar grupo muscular
 Route::resource('/gmuscular','GmuscularController');
