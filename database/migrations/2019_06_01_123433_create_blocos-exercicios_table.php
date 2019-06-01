62748705cde82371b7691d67b144f7c8c5e0f76f
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-06-01 11:04:22 -0300

    #42 corrigindo erro de sintaxe da tabela blocos de exercicios

--- database/migrations/2019_06_01_123433_create_blocos-exercicios_table.php
+++ database/migrations/2019_06_01_123433_create_blocos-exercicios_table.php
@@ -13,10 +13,10 @@ class CreateBlocosExerciciosTable extends Migration
      */
     public function up()
     {
-        Schema:create('blocos-exercicios', function (Blueprint $table){
+        Schema::create('blocos-exercicios', function (Blueprint $table){
             $table->increments('id');
             $table->string('nome');
-        };
+        });
     }
 
     /**
@@ -26,6 +26,6 @@ class CreateBlocosExerciciosTable extends Migration
      */
     public function down()
     {
-        Schema:dropIfExists('blocos-exercicios');
+        Schema::dropIfExists('blocos-exercicios');
     }
 }
