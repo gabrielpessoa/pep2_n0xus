db511224d98a4cfc18f7883da46583ff36910738
luizcsrj <luizcarlosjr1999@gmail.com>
2019-06-04 15:21:51 -0300

    #38 criando usercontroller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        }
+    public function index()
+    {
+        $users = User::all();
+        return view('user.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'data_nascimento' => 'required',
+            'endereco' => 'required',
+            'telefone' => 'required',
+            'profissao' => 'required',
+            'queixa' => 'required',
+            'patologia' => 'required',
+            'data_pagamento' => 'required',
+   
+        ]);
+
+        User::create($request->all());
+        
+        return redirect()->route('user.index')
+                        ->with('success','Aluno criado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $users = User::find($id);
+        return view('user.show', compact('users'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $users = User::find($id);
+        return view('user.edit', compact('users'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'data_nascimento' => 'required',
+            'endereco' => 'required',
+            'telefone' => 'required',
+            'profissao' => 'required',
+            'queixa' => 'required',
+            'patologia' => 'required',
+            'data_pagamento' => 'required',
+        
+        ]);
+  
+        $user = User::find($id);
+        $user->update($request->all());
+  
+        return redirect()->route('user.index')
+                        ->with('success','Dados do aluno editado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $delete = User::find($id);
+        $delete->delete();
+        return redirect()->route('user.index')
+                        ->with('success','Dados do aluno deletado com sucesso!');
+    }
+}
