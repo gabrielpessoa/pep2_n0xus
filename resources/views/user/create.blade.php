e8276ae34c88557b3861f3a37258a02d08250127
luizcsrj <luizcarlosjr1999@gmail.com>
2019-06-04 15:20:25 -0300

    38 criando pasta user

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,64 @@
+@extends('adminlte::page')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Adicionar novo aluno </h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('user.index') }}"> Voltar</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Erro! </strong> Tem alguns problemas no seu campo. Preencha corretamente<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('user.store') }}" method="POST">
+    @csrf
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Aluno*:</strong>
+                <input type="text" name="user" class="form-control">
+            </div>
+            <div class="form-group">
+                <strong>Data de nascimento*:</strong>
+                <input type="text" name="data_nascimento" class="form-control">
+            </div>
+            <div class="form-group">
+                <strong>Endereço*:</strong>
+                <input type="text" name="endereco" class="form-control">
+            </div>
+            <div class="form-group">
+                <strong>Profissão*:</strong>
+                <input type="text" name="profissao" class="form-control">
+            </div>
+            <div class="form-group">
+                <strong>Queixa*:</strong>
+                <input type="text" name="queixa" class="form-control">
+            </div>
+            <div class="form-group">
+                <strong>Patologia*:</strong>
+                <input type="text" name="patologia" class="form-control">
+            </div>
+            <div class="form-group">
+                <strong>Data de pagamento*:</strong>
+                <input type="text" name="data_pagamento" class="form-control">
+            </div>
+        </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Adicionar</button>
+    </div>
+</form>
+
+@endsection
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('adminlte::page')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Editar</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('user.index') }}"> Voltar</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('user.update',$users->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Aluno*:</strong>
+                    <input type="text" name="name" value="{{ $users->user }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <strong>Data de nascimento*:</strong>
+                    <input type="text" name="'data_nascimento'" value="{{ $users->user }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <strong>Endereço*:</strong>
+                    <input type="text" name="endereco" value="{{ $users->user }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <strong>Profissão*:</strong>
+                    <input type="text" name="profissao" value="{{ $users->user }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <strong>Queixa*:</strong>
+                    <input type="text" name="queixa" value="{{ $users->user }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <strong>Patologia*:</strong>
+                    <input type="text" name="patologia" value="{{ $users->user }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <strong>data de pagamento*:</strong>
+                    <input type="text" name="data_pagamento" value="{{ $users->user }}" class="form-control">
+                </div>
+            </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
