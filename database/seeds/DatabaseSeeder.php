3193af2a31150939d18c4f2181d2254655dc2222
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-06-06 09:09:07 -0300

    #48 adicionando seeder de blocos de exercicios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,6 +21,23 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => 1
         ]);
+	
+	 DB::table('blocos_exercicios')->insert([
+            'nome' => 'Alongamento',
+       
+        ]);
+	 DB::table('blocos_exercicios')->insert([
+            'nome' => 'Tronco',
+       
+        ]);
+	 DB::table('blocos_exercicios')->insert([
+            'nome' => 'MMII',
+       
+        ]);
+	 DB::table('blocos_exercicios')->insert([
+            'nome' => 'MMSS',
+       
+        ]);
     }
     
 }
