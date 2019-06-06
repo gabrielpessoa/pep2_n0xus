05041cf35e5af3118bc8f1030e2ad496ea38c73b
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-06-06 07:38:13 -0300

    #38 tirando o create da factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory('App\User', 50)->create();
+       
         User::create([
             'name' => 'Admin',
             'email' => 'Admin@Admin.com',
