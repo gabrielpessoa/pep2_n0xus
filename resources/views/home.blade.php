82cb02729f7f10cd85ce2d9f64b481144e0810fa
arthurviniciuse <arthur.alucard41@gmail.com>
2019-06-12 23:43:34 -0300

    #45 tabela de agendamento v1.00

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,55 @@
 @section('title', 'STUDIO')
 
 @section('content')
-    <p>BEM VINDO!</p>
+<form>
+<div class="form-group">
+    <label for="exampleFormControlSelect1">Dia da Semana</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Segunda</option>
+      <option>Terça-Feira</option>
+      <option>Quarta-Feira</option>
+      <option>Quinta-Feira</option>
+      <option>Sexta-Feira</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Selecionar Turma</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Turma 1</option>
+      <option>Turma 2</option>
+      <option>Turma 3</option>
+      <option>Turma 4</option>
+      <option>Turma 5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Horário</label>
+    <input type="time" class="form-control" id="exampleFormControlInput1">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Grupo muscular</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Grupo muscular 1</option>
+      <option>Grupo muscular 2</option>
+      <option>Grupo muscular 3</option>
+      <option>Grupo muscular 4</option>
+      <option>Grupo muscular 5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Equipamento</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Equipamento 1</option>
+      <option>Equipamento 2</option>
+      <option>Equipamento 3</option>
+      <option>Equipamento 4</option>
+      <option>Equipamento 5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <button>Agendar</button>
+  </div>
+  
+</form>
+    
 @stop
