867c209a4625185632d5f77c2debce497b54235d
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-06-18 22:34:39 -0300

    #31 ajustando input de hora de turma no create

--- resources/views/turma/create.blade.php
+++ resources/views/turma/create.blade.php
@@ -35,7 +35,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Horário*:</strong>
-                <input type="text" name="horario" class="form-control"> </div>
+                <input type="time" name="horario" class="form-control"> </div>
         </div>
         </div>
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
