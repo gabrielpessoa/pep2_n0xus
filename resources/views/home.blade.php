d3c550277638a71d8d8b5e8dde86ae246e427f04
Guilherme <gui.gcs13@gmail.com>
2019-07-01 17:45:02 -0300

    Alteracoes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,42 +3,35 @@
 @section('title', 'STUDIO')
 
 @section('content')
-<table>
-<form>
-
-  <tr>
-    <td>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Blocos de Exercícios</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      @foreach ($blocos as $bloco)
-      <option value="{{$bloco-> id=2}}">{{$bloco->nome}}</option>
-      @endforeach
-    </select>
-  </div>
-</td>
-  <td>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Grupo muscular</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      @foreach ($gmusculares as $gmuscular)
-      <option value="{{$gmuscular-> id}}">{{$gmuscular->nome}}</option>
-      @endforeach
-    </select>
-  </div>
-</td>
-<td>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Equipamento</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      @foreach ($equipamentos as $equipamento)
-      <option value="{{$equipamento-> id}}">{{$equipamento->equipamento}}</option>
-      @endforeach
-    </select>
-  </div>
-   </td>
-</table> 
-    <div class="col-xs-6 col-sm-6 col-md-6 text-center">
-                <button type="submit" class="btn btn-primary">Agendar</button>
+  <div class="container">
+<form method="post" action="{{route(home.store)}}">
+@for($i=0; $i<4; $i++)
+  
+    <div class="row">
+      <div class="col-6">
+        <label for="exampleFormControlSelect1">Blocos de Exercícios</label> 
+        <input type="text" name="bloco" disabled="" value= "{{$blocos[$i]->nome}}">
+      </div>
+      <div class="col-6">
+        <label for="exampleFormControlSelect1">Grupo muscular</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          @foreach ($gmusculares as $gmuscular)
+          <option value="{{$gmuscular-> id}}">{{$gmuscular->nome}}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="col-6">
+        <label for="exampleFormControlSelect1">Equipamento</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          @foreach ($equipamentos as $equipamento)
+          <option value="{{$equipamento-> id}}">{{$equipamento->equipamento}}</option>
+          @endforeach
+        </select>
+      </div>
     </div>
+  @endfor
+    <div class="col-xs-6 col-sm-6 col-md-6 text-center">
+      <button type="submit" class="btn btn-primary">Agendar</button>
+    </div>
+  </div>
 @stop
